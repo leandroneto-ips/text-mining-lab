320ccf87d7a04032815277ce82caf54d242b821b
leandroneto-ips <2024139913@estudantes.ips.pt>
2025-03-23 01:02:15 +0000

    added function currDateTime for memory tracking logs

--- utils/memoryMonitor.js
+++ utils/memoryMonitor.js
@@ -5,16 +5,21 @@ const formatToMBLabel = (number, decimalPoints = 2) => {
   return (number / 1024 /1024).toFixed(decimalPoints) + ' MB';
 };
 
+const currDateTime = () => {
+    return new Date().toISOString().substring(0, 19);
+};
+
 const trackMemory = (intervalSeconds = 5) => {
     setInterval(async () => {
         const mysqlMemoryUsage = await dbMemoryUsage.getDBMemoryUsage();
         const nodeMemoryUsage = process.memoryUsage();
-        console.log(`[ Memory Usage ---> | nodeHeapTotal: ${formatToMBLabel(nodeMemoryUsage.heapTotal)} | nodeHeapUsed: ${formatToMBLabel(nodeMemoryUsage.heapUsed)} | nodeRss: ${formatToMBLabel(nodeMemoryUsage.rss)} | mySqlTotal: '${mysqlMemoryUsage.value_in_MB}' | ]`);
+        console.log(`[ Memory Usage @${currDateTime()} ::: | nodeRss: ${formatToMBLabel(nodeMemoryUsage.rss)} | mySqlTotal: '${mysqlMemoryUsage.value_in_MB}' | ]`);
     }, intervalSeconds * 1000);
 }
 
 module.exports = {
-    trackMemory
+    trackMemory,
+    currDateTime
 };
 
 
